fb21e25740f9e8af7c77e63a1fcc33f5f650f333
Fredrik Andersson <fredrik.andersson1@chasacademy.se>
2024-10-17 13:54:28 +0200

    ändrade namn på variablar numb och började på if/else switch

--- kalkylator.js
+++ kalkylator.js
@@ -1,6 +1,6 @@
 console.log("Hello World!");
 
-let numbOneUndefined = Number(
+let numbOne = Number(
   prompt("Hej och välkommen till kalkylatorn: Skriv in önskat tal")
 );
 
@@ -8,6 +8,18 @@ let value = prompt(
   "Addition (+), subtraktion (-), multiplikation (*), division (/), modulus (%)"
 );
 
-let numbTwoUndefined = Number(prompt("Skriv in ett till tal"));
+let numbTwo = Number(prompt("Skriv in ett till tal"));
 
 let resultat = 0;
+
+if (isNaN(numbOne) || isNaN(numbTwo)) {
+  console.log("Du måste skriva in nummer.");
+  alert("Du måste skriva in ett nummer.");
+} else
+  switch (value) {
+    case "+":
+      resultat = numbOne + numbTwo;
+      break;
+  }
+console.log("resultat: ", resultat);
+alert(`Resultat ${resultat}`);
