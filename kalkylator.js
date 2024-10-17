f0b2626d087f4ff2b7e541f06afc882162e0bfb9
Fredrik Andersson <fredrik.andersson1@chasacademy.se>
2024-10-17 13:46:25 +0200

    lagt in variabler med prompt

--- kalkylator.js
+++ kalkylator.js
@@ -1,1 +1,13 @@
 console.log("Hello World!");
+
+let numbOneUndefined = Number(
+  prompt("Hej och välkommen till kalkylatorn: Skriv in önskat tal")
+);
+
+let value = prompt(
+  "Addition (+), subtraktion (-), multiplikation (*), division (/), modulus (%)"
+);
+
+let numbTwoUndefined = Number(prompt("Skriv in ett till tal"));
+
+let resultat = 0;
