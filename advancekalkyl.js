d932880e0d2f86c72f32ada295b20cb4292aa088
Fredrik Andersson <fredrik.andersson1@chasacademy.se>
2024-10-17 14:14:23 +0200

    avancerad

---
+++ advancekalkyl.js
@@ -0,0 +1,57 @@
+// Tar emot användaren tal
+let numbOne = Number(
+  prompt(
+    "Hej och välkommen till kalkylatorn: Skriv in önskat tal mellan 0 - 100"
+  )
+);
+// Tar emot användaren operatör
+let value = prompt(
+  "Addition (+), subtraktion (-), multiplikation (*), division (/), modulus (%)"
+);
+// Tar emot användarens andra Tal
+let numbTwo = Number(prompt("Skriv in ett till tal mellan 0 - 100"));
+// resultat Undefined
+let resultat = 0;
+
+// if/else switch för att använda value.
+
+if (isNaN(numbOne) || isNaN(numbTwo)) {
+  console.log("Du måste skriva in nummer.");
+  alert("Du måste skriva in ett nummer.");
+} else
+  switch (value) {
+    case "+":
+      resultat = numbOne + numbTwo;
+      console.log("resultat: ", resultat);
+      alert(`Resultat ${resultat}`);
+      break;
+    case "-":
+      resultat = numbOne - numbTwo;
+      console.log("resultat: ", resultat);
+      alert(`Resultat ${resultat}`);
+      break;
+    case "/":
+      resultat = numbOne / numbTwo;
+      console.log("resultat: ", resultat);
+      alert(`Resultat ${resultat}`);
+      break;
+    case "*":
+      resultat = numbOne * numbTwo;
+      console.log("resultat: ", resultat);
+      alert(`Resultat ${resultat}`);
+      break;
+    case "%":
+      resultat = numbOne % numbTwo;
+      console.log("resultat: ", resultat);
+      alert(`Resultat ${resultat}`);
+      break;
+    case "procent":
+      resultat = numbOne * 0.01 * numbTwo;
+      console.log("resultat: ", resultat);
+      alert(`Resultat ${resultat}`);
+      break;
+    default:
+      console.log("felaktigt operatör.");
+      alert("Felaktig operatör!");
+      break;
+  }
