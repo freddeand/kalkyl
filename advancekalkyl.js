576d75c944510bd86e19fd2fb04c78f3105b7bad
Fredrik Andersson <fredrik.andersson1@chasacademy.se>
2024-10-25 13:28:18 +0200

    fixade en array som sparar resultat och visar det i prompt

--- advancekalkyl.js
+++ advancekalkyl.js
@@ -1,3 +1,5 @@
+// let history = [];
+// let historyTwo = history.slice(3);
 function startLoop() {
   do {
     choice = Number(
@@ -20,6 +22,8 @@ function goToYouTube() {
   console.log(window.location);
 }
 // numbOne inputs the users number, value inputs the users operator, numbTwo inputs the users number
+// array for result history.
+let history = [];
 
 function advanceCalculator() {
   let isRunning = true;
@@ -34,7 +38,7 @@ function advanceCalculator() {
       "1. Addition (+)\n 2. subtraktion (-)\n 3. division (/)\n 4. multiplikation (*)\n 5. modulus (%)\n 6. Upphöjt (^)"
     );
     let numbTwo = parseFloat(prompt("Skriv in ett till."));
-
+    let symbol;
     let result;
     let validResult = true;
     // if/else switch to evaluate the users value.
@@ -46,12 +50,15 @@ function advanceCalculator() {
     } else
       switch (value) {
         case "1":
+          symbol = "+";
           result = numbOne + numbTwo;
           break;
         case "2":
+          symbol = "-";
           result = numbOne - numbTwo;
           break;
         case "3":
+          symbol = "/";
           if (numbTwo === 0) {
             console.log("Du kan inte dela med noll.");
             alert("Du kan inte dela med noll.");
@@ -61,9 +68,11 @@ function advanceCalculator() {
           }
           break;
         case "4":
+          symbol = "*";
           result = numbOne * numbTwo;
           break;
         case "5":
+          symbol = "%";
           if (numbTwo === 0) {
             console.log("Du kan inte modulus med noll.");
             alert("Du kan inte modulus med noll.");
@@ -73,6 +82,7 @@ function advanceCalculator() {
           }
           break;
         case "6":
+          symbol = "^";
           result = numbOne ** numbTwo;
           break;
 
@@ -95,8 +105,21 @@ function advanceCalculator() {
       // show result and helps you choose if you want to continue calculating or end the calculator.
       console.log("Resultat: ", result);
       alert(`Resultat ${result}`);
+
+      history.push(
+        `Du har skrivit in ${numbOne} ${symbol} ${numbTwo} = ${result}\n`
+      );
+      const historyTwo = history.slice(-3);
+      console.log(history);
+      for (let i = 0; i < history.length; i++) {}
+
       isRunning = false;
-      let userContinue = prompt("Vill du forsätta? skriv in ja eller nej");
+      let userContinue = prompt(
+        "Tidigare beräkningar\n" +
+          historyTwo +
+          "\nVill du forsätta? skriv in ja eller nej"
+      );
+
       if (userContinue.toLowerCase() === "ja") {
         advanceCalculator();
       } else {
