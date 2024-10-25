e486829fc3bd929f9851bc61848d5002d6a3e879
Fredrik Andersson <fredrik.andersson1@chasacademy.se>
2024-10-25 11:41:21 +0200

    ternary op på första loopen, loop fixad på kalkylatorn.

--- advancekalkyl.js
+++ advancekalkyl.js
@@ -2,96 +2,106 @@ function startLoop() {
   do {
     choice = Number(
       prompt(
-        "Välj ett av dessa alternativ: \n1: Starta Kalkylatorn, \n2: Kolla på youtube, \n3: Avsluta"
+        "Välj ett av dessa alternativ: \n1: Starta Kalkylatorn \n2: Kolla på youtube \n3: Avsluta"
       )
     );
-    if (choice === 1) {
-      avanceradKalkylator();
-    } else if (choice === 2) {
-      goToYouTube();
-    } else if (choice === 3) {
-    } else {
-    }
+    choice === 1
+      ? advanceCalculator()
+      : choice === 2
+      ? goToYouTube()
+      : choice !== 3
+      ? console.log("ERROR: Du måste skriva in ett tal mellan 1-3")
+      : null;
   } while (choice !== 3);
 }
+// Sends user to youtube to learn about basic math.
 function goToYouTube() {
   window.location = "https://www.youtube.com/watch?v=TMubSggUOVE";
   console.log(window.location);
 }
+// numbOne inputs the users number, value inputs the users operator, numbTwo inputs the users number
 
-function avanceradKalkylator() {
-  // Tar emot användaren tal
-  let numbOne = parseFloat(
-    prompt(
-      "Hej och välkommen till kalkylatorn: Skriv in önskat tal mellan 0 - 100"
-    )
-  );
-  // Tar emot användaren operatör
-  let value = prompt(
-    "1. Addition (+)\n 2. subtraktion (-)\n 3. multiplikation (*)\n 4. division (/)\n 5. modulus (%)\n 6. Upphöjt (^)"
-  );
-  // Tar emot användarens andra Tal
-  let numbTwo = parseFloat(prompt("Skriv in ett till tal mellan 0 - 100"));
-  // console.log("isnan", isNaN(numbTwo), numbTwo);
+function advanceCalculator() {
+  let isRunning = true;
 
-  let resultat;
-  let giltigtResultat = true;
-  // if/else switch för att använda value.
+  while (isRunning) {
+    let numbOne = parseFloat(
+      prompt(
+        "Hej och välkommen till kalkylatorn: Skriv in önskat tal för att utföra en uträkning."
+      )
+    );
+    let value = prompt(
+      "1. Addition (+)\n 2. subtraktion (-)\n 3. division (/)\n 4. multiplikation (*)\n 5. modulus (%)\n 6. Upphöjt (^)"
+    );
+    let numbTwo = parseFloat(prompt("Skriv in ett till."));
 
-  if (isNaN(numbOne) || isNaN(numbTwo)) {
-    console.log("Du måste skriva in nummer.");
-    alert("Du måste skriva in ett nummer.");
-    giltigtResultat = false;
-  } else
-    switch (value) {
-      case "1":
-        resultat = numbOne + numbTwo;
-        break;
-      case "2":
-        resultat = numbOne - numbTwo;
-        break;
-      case "3":
-        if (numbTwo === 0) {
-          console.log("Du kan inte dela med noll.");
-          alert("Du kan inte dela med noll.");
-          giltigtResultat = false;
-        } else {
-          resultat = numbOne / numbTwo;
-        }
-        break;
-      case "4":
-        resultat = numbOne * numbTwo;
-        break;
-      case "5":
-        if (numbTwo === 0) {
-          console.log("Du kan inte modulus med noll.");
-          alert("Du kan inte modulus med noll.");
-          giltigtResultat = false;
-        } else {
-          resultat = numbOne % numbTwo;
-        }
-        break;
-      case "6":
-        resultat = numbOne ^ numbTwo;
-        break;
-      default:
-        console.log("felaktigt operatör.");
-        alert("Felaktig operatör!");
-        giltigtResultat = false;
-        break;
+    let result;
+    let validResult = true;
+    // if/else switch to evaluate the users value.
+
+    if (isNaN(numbOne) || isNaN(numbTwo)) {
+      console.log("Du måste skriva in nummer.");
+      alert("Du måste skriva in ett nummer.");
+      validResult = false;
+    } else
+      switch (value) {
+        case "1":
+          result = numbOne + numbTwo;
+          break;
+        case "2":
+          result = numbOne - numbTwo;
+          break;
+        case "3":
+          if (numbTwo === 0) {
+            console.log("Du kan inte dela med noll.");
+            alert("Du kan inte dela med noll.");
+            validResult = false;
+          } else {
+            result = numbOne / numbTwo;
+          }
+          break;
+        case "4":
+          result = numbOne * numbTwo;
+          break;
+        case "5":
+          if (numbTwo === 0) {
+            console.log("Du kan inte modulus med noll.");
+            alert("Du kan inte modulus med noll.");
+            validResult = false;
+          } else {
+            result = numbOne % numbTwo;
+          }
+          break;
+        case "6":
+          result = numbOne ** numbTwo;
+          break;
+
+        default:
+          console.log("felaktigt operatör.");
+          alert("Felaktig operatör!");
+          validResult = false;
+          break;
+      }
+
+    if (validResult) {
+      // Kollar om resultatet är utan decimaler, avrundar annars till 2 decimaler.
+      let resultNoDec = parseFloat(result.toFixed(0));
+      if (result === resultNoDec) {
+        // Är redan heltal avrunda inte till två decimaler.
+      } else {
+        result = result.toFixed(2);
+      }
+
+      // show result and helps you choose if you want to continue calculating or end the calculator.
+      console.log("Resultat: ", result);
+      alert(`Resultat ${result}`);
+      isRunning = false;
+      let userContinue = prompt("Vill du forsätta? skriv in ja eller nej");
+      if (userContinue.toLowerCase() === "ja") {
+        advanceCalculator();
+      } else {
+        console.log("Du måste srkiva in ja eller nej.");
+      }
     }
-
-  if (giltigtResultat) {
-    // Kollar om resultatet är utan decimaler, avrundar annars till 2 decimaler.
-    let resultatUtanDecimaler = parseFloat(resultat.toFixed(0));
-    if (resultat === resultatUtanDecimaler) {
-      // Är redan heltal avrunda inte till två decimaler.
-    } else {
-      resultat = resultat.toFixed(2);
-    }
-
-    // Visa resultat
-    console.log("Resultat: ", resultat);
-    alert(`Resultat ${resultat}`);
   }
 }
