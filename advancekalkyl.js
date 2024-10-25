dcf23f92ce65e8f721e56d13c0ddc117c6027561
Benjamin Stenlund <benjamin.stenlund@chasacademy.se>
2024-10-25 14:45:43 +0200

    Fixade till så det går avbryta med avbryt-knappen.

--- advancekalkyl.js
+++ advancekalkyl.js
@@ -1,19 +1,22 @@
-// let history = [];
-// let historyTwo = history.slice(3);
 function startLoop() {
+  const menuText = "Välj ett av dessa alternativ: \n1: Starta Kalkylatorn \n2: Kolla på youtube \n3: Avsluta";
+  const errorText = "Du skrev fel. Det måste vara en siffra mellan 1 och 3."
+  let promptText = menuText;
   do {
-    choice = Number(
-      prompt(
-        "Välj ett av dessa alternativ: \n1: Starta Kalkylatorn \n2: Kolla på youtube \n3: Avsluta"
-      )
-    );
+    choice = Number(prompt(promptText));
+
     choice === 1
       ? advanceCalculator()
       : choice === 2
-      ? goToYouTube()
-      : choice !== 3
-      ? console.log("ERROR: Du måste skriva in ett tal mellan 1-3")
-      : null;
+        ? goToYouTube()
+        : choice === 0
+          ? choice = 3
+          : promptText = errorText.toUpperCase() + "\n\n" + menuText; console.log("ERROR: Du måste skriva in ett tal mellan 1-3")
+          ;
+          
+          
+    console.log(choice);
+          
   } while (choice !== 3);
 }
 // Sends user to youtube to learn about basic math.
@@ -34,10 +37,20 @@ function advanceCalculator() {
         "Hej och välkommen till kalkylatorn: Skriv in önskat tal för att utföra en uträkning."
       )
     );
+    if (numbOne === NaN){
+      return;
+    }
     let value = prompt(
-      "1. Addition (+)\n 2. subtraktion (-)\n 3. division (/)\n 4. multiplikation (*)\n 5. modulus (%)\n 6. Upphöjt (^)"
+      `Du har skrivit in ${numbOne}\nVälj någon av operatorerna\n1. Addition (+)\n 2. subtraktion (-)\n 3. division (/)\n 4. multiplikation (*)\n 5. modulus (%)\n 6. Upphöjt (^)`
     );
+    if (value === null) {
+      return;
+    }
     let numbTwo = parseFloat(prompt("Skriv in ett till."));
+    if (numbTwo === NaN){
+      return;
+    }
+    
     let symbol;
     let result;
     let validResult = true;
@@ -111,19 +124,31 @@ function advanceCalculator() {
       );
       const historyTwo = history.slice(-3);
       console.log(history);
-      for (let i = 0; i < history.length; i++) {}
 
-      isRunning = false;
+      // Alternative solution to get the last three items just to have a for-loop.
+      // let i = history.length - 3;
+      // if (i < 0) {
+      //   i = 0;
+      // }
+      // for (; i < history.length; i++) {
+      //   console.log(`${history[i]}`);
+      // }
+
+      // isRunning = false;
       let userContinue = prompt(
         "Tidigare beräkningar\n" +
-          historyTwo +
-          "\nVill du forsätta? skriv in ja eller nej"
+        historyTwo +
+        "\nVill du forsätta? skriv in ja eller nej"
       );
 
       if (userContinue.toLowerCase() === "ja") {
         advanceCalculator();
+      } else if (userContinue === null) {
+        // return;
+        isRunning = false;
       } else {
-        console.log("Du måste srkiva in ja eller nej.");
+        alert("Du måste skriva in ja eller nej.")
+        console.log("Du måste skriva in ja eller nej.");
       }
     }
   }
